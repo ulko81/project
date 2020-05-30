e3849de9ae93257545b8d033355bcdbb57248731
ulko81 <zmeyko81@ukr.net>
2020-05-30 22:25:28 +0300

    new_file

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
 import os
-print('Hello world', os.environ['HOME'])
+
+driver_name = {'chrome': 'chromedriver.exe',
+               'firefox': 'geckodriver.exe'
+               }
+
+drivers_dir = {'windows': os.path.abspath('../drivers/'),
+               'linux': '/home/travis/virtualenv/python3.6.7/bin/'
+               }
+
+r = os.path.join(os.environ['HOME'], drivers_dir['linux'], driver_name['chrome'][:-4])
+print(r)
