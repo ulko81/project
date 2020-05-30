0196d1b0081fd5839ac369cd6c07b554d515eb48
ulko81 <zmeyko81@ukr.net>
2020-05-30 22:40:39 +0300

    new_file

--- hello.py
+++ hello.py
@@ -12,5 +12,5 @@ drivers_dir = {'windows': os.path.abspath('../drivers/'),
                'linux': '/virtualenv/python3.6.7/bin/'
                }
 
-r = os.path.join(os.environ['HOME'], drivers_dir['linux'], driver_name[get_browser()][:-4])
+r = os.path.join(os.environ['HOME'], driver_name[get_browser()][:-4])
 print(r)
