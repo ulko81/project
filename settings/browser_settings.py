6518eee82f954f9ce8e4c2cc68c3b0bac46f99c3
ulko81 <zmeyko81@ukr.net>
2020-05-30 22:33:57 +0300

    new_file

--- settings/browser_settings.py
+++ settings/browser_settings.py
@@ -13,7 +13,7 @@ class Browser:
                   }
 
     drivers_dir = {'windows': os.path.abspath('../drivers/'),
-                   'linux': '/home/travis/virtualenv/python3.6.7/bin/'
+                   'linux': '/virtualenv/python3.6.7/bin/'
                   }
 
     def get_driver_path(self):
