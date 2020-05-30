0b9cafb0314ea348ef83e7bf448ee075324e9953
ulko81 <zmeyko81@ukr.net>
2020-05-30 08:44:45 +0300

    new_file

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -15,3 +15,18 @@ class TestCart:
         assert product_card.check_present_green_button_buy()
         assert '1' == header_cart.text_digit_cart_header()
         assert product_card.text_card_price().lower() == header_cart.text_cart_header_price().lower()
+
+    def test_check_cart_in_header_add_from_product_card(self):
+        header_cart = HeaderPage(self.driver)
+        product_card = ProductCardPage(self.driver)
+        self.driver.get(TEST_URL + PRODUCT_CARD_LINK)
+        product_card.click_button_buy()
+        assert product_card.check_present_green_button_buy()
+        assert '1' == header_cart.text_digit_cart_header()
+        assert product_card.text_card_price().lower() == header_cart.text_cart_header_price().lower()
+
+    def test_cart_in_header_add_from_product_card(self):
+        assert(1==1)
+
+    def test_in_header_add_from_product_card(self):
+        assert(2==2)
