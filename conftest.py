ed64ab51309c04d5a3aa3932730b732c271856e5
ulko81 <zmeyko81@ukr.net>
2020-06-03 15:52:13 +0300

    new_file

--- conftest.py
+++ conftest.py
@@ -13,4 +13,14 @@ def get_driver(request):
         driver.quit()
     request.addfinalizer(close_driver)
 
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item, call):
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+    if report.when == 'call':
+        extra.append(pytest_html.extras.html('<p>some html</p>'))
+        report.extra = extra
+
 
