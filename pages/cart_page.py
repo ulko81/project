99be303f8f2540d968d5daf61c703c569c8aa224
ulko81 <zmeyko81@ukr.net>
2020-06-04 08:54:59 +0300

    new_file

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -7,6 +7,6 @@ class CartPage(BasePage, CartLocator):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def text_count_in_icon_cart(self, driver):
-        return self.get_web_element(driver, self.cart_count).text
+    def text_count_in_icon_cart(self):
+        return self.get_web_element(self.cart_count).text
 
