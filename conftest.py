8c9c12804caa690278ba9449c3a17ac77e36551d
ulko81 <zmeyko81@ukr.net>
2020-06-19 18:31:21 +0300

    new_tests

--- conftest.py
+++ conftest.py
@@ -18,21 +18,24 @@ def get_driver(request):
 def delay():
     time.sleep(10)
 
+from datetime import datetime
 import pytest
+
+def pytest_html_results_table_header(cells):
+    cells.insert(2, html.th('Description'))
+    cells.insert(1, html.th('Time', class_='sortable time', col='time'))
+    cells.pop()
+
+def pytest_html_results_table_row(report, cells):
+    cells.insert(2, html.td(report.description))
+    cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
+    cells.pop()
+
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
-    pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
     report = outcome.get_result()
-    extra = getattr(report, 'extra', [])
-    if report.when == 'call':
-        # always add url to report
-        extra.append(pytest_html.extras.url('http://www.example.com/'))
-        xfail = hasattr(report, 'wasxfail')
-        if (report.skipped and xfail) or (report.failed and not xfail):
-            # only add additional html on failure
-            extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
-        report.extra = extra
+    report.description = str(item.function.__doc__)
 
 
 
