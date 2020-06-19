2968ef25de77ac560221395886c40a0684c7458e
ulko81 <zmeyko81@ukr.net>
2020-06-19 18:17:52 +0300

    new_tests

--- conftest.py
+++ conftest.py
@@ -18,5 +18,21 @@ def get_driver(request):
 def delay():
     time.sleep(10)
 
+import pytest
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+    if report.when == 'call':
+        # always add url to report
+        extra.append(pytest_html.extras.url('http://www.example.com/'))
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # only add additional html on failure
+            extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+        report.extra = extra
+
 
 
