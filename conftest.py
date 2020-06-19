082943850a6e6aec6bb4764e020412183cb1555a
ulko81 <zmeyko81@ukr.net>
2020-06-19 18:36:47 +0300

    new_tests

--- conftest.py
+++ conftest.py
@@ -14,28 +14,22 @@ def get_driver(request):
         driver.quit()
     request.addfinalizer(close_driver)
 
-@pytest.fixture()
-def delay():
-    time.sleep(10)
 
-from datetime import datetime
 import pytest
-
-def pytest_html_results_table_header(cells):
-    cells.insert(2, html.th('Description'))
-    cells.insert(1, html.th('Time', class_='sortable time', col='time'))
-    cells.pop()
-
-def pytest_html_results_table_row(report, cells):
-    cells.insert(2, html.td(report.description))
-    cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
-    cells.pop()
-
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
+    pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
     report = outcome.get_result()
-    report.description = str(item.function.__doc__)
+    extra = getattr(report, 'extra', [])
+    if report.when == 'call':
+        # always add url to report
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # only add additional html on failure
+            extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+        report.extra = extra
 
-
+def pytest_html_report_title(report):
+   report.title = "My very own title!"
 
