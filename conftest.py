b6ee476fcb32545235e6e65555b76e9ecc7e009d
ulko81 <zmeyko81@ukr.net>
2020-06-21 20:21:35 +0300

    new_tests

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from settings.browser_setting import Browser
 import pytest
-from  py._xmlgen import html
+from py._xmlgen import html
 
 
 browser = {
@@ -8,10 +9,12 @@ browser = {
     'firefox':Browser().set_firefox
 }
 
+
 @pytest.fixture()
 def get_driver(request):
     driver = browser[Browser().get_browser]()
     request.cls.driver = driver
+
     def close_driver():
         driver.quit()
     request.addfinalizer(close_driver)
@@ -21,5 +24,19 @@ def pytest_html_report_title(report):
    report.title = "Report From "
 
 def pytest_html_results_table_header(cells):
-    cells.insert(1, html.th('Ticket'))
+    cells.insert(2, html.th('Description'))
+    cells.insert(1, html.th('Time', class_='sortable time', col='time'))
+    cells.pop()
 
+
+def pytest_html_results_table_row(report, cells):
+    cells.insert(2, html.td(report.description))
+    cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
+    cells.pop()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    report.description = str(item.function.__doc__)
