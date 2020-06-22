0de6950b27b7ddca0d778b6865de614396594353
ulko81 <zmeyko81@ukr.net>
2020-06-22 10:03:45 +0300

    new_tests

--- conftest.py
+++ conftest.py
@@ -24,21 +24,3 @@ def pytest_html_report_title(report):
    report.title = "Report From "
 
 
-def pytest_html_results_table_header(cells):
-    cells.insert(2, html.th('Description'))
-    cells.insert(1, html.th('Time', class_='sortable time', col='time'))
-    cells.pop()
-    cells.insert
-
-
-def pytest_html_results_table_row(report, cells):
-    cells.insert(2, html.td(report.description))
-    cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
-    cells.pop()
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    report = outcome.get_result()
-    report.description = str(item.function.__doc__)
